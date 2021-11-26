f9f2f47b65fdf484989f032938c6cde1e2344469
Genevive Alfirevic <gen@nyble.dev>
2021-11-26 10:25:02 -0600

    Messy conf based epoch

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,45 @@
 use arvelie::Arvelie;
+use confindent::Confindent;
+use dirs::config_dir;
 use time::Date;
 
 fn main() {
 	let args: Vec<String> = std::env::args().skip(1).collect();
 
 	match args.len() {
+		0 => {
+			let mut config_path = config_dir().unwrap();
+			config_path.push("arvelie.conf");
+
+			let conf = match Confindent::from_file(&config_path) {
+				Ok(cnfi) => cnfi,
+				Err(e) => {
+					eprintln!(
+						"Couldn't read config at '{}': {}",
+						config_path.to_string_lossy(),
+						e
+					);
+					return;
+				}
+			};
+
+			let epoch: i32 = match conf.child_parse("EpochYear") {
+				Ok(e) => e,
+				Err(e) => {
+					eprintln!(
+						"Couldn't parse EpochYear from '{}': {}",
+						config_path.to_string_lossy(),
+						e
+					);
+					return;
+				}
+			};
+
+			println!(
+				"{}",
+				Arvelie::from_utc(Date::try_from_yo(epoch, 1).unwrap()).as_string()
+			)
+		}
 		1 => {
 			println!(
 				"{}",
@@ -23,7 +58,10 @@ fn main() {
 			);
 		}
 		_ => {
-			println!("Usage: arvelie [epoch_year] [date]\nIf no date is provided, the current date in UTC is returned.");
+			let mut config_path = config_dir().unwrap();
+			config_path.push("arvelie.conf");
+
+			println!("Usage: arvelie [epoch_year] [date]\nIf no epoch_year is provided, its read from {}\nIf no date is provided, the current date in UTC is returned.", config_path.to_string_lossy());
 		}
 	}
 }
